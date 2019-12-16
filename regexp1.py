9c0ea6db4ed6c766ff73678156c368192c5ea567
Yogesh Sharma <yogeshsharma1981@gmail.com>
2019-12-16 14:22:18 -0500

    commit

--- regexp1.py
+++ regexp1.py
@@ -1,5 +1,17 @@
-hand = open('mshort-box.txt')
+hand = open('mbox-short.txt')
+print(hand)
 for line in hand:
     line = line.rstrip()
-    if line.find('From:') >=0
-    print(line)
+    if line.find('From:') >=0 :
+     print(line)
+
+
+
+import re 
+
+hand = open('mbox-short.txt')
+print(hand)
+for line in hand:
+    line = line.rstrip()
+    if re.search('From:', line):
+     print(line)
